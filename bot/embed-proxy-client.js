f57e4d0336b265d2eeb26d8e2b755db652ea2471
Arc <90333108+lastarc@users.noreply.github.com>
2024-04-27 20:07:24 +0200

    feat(bot): add jsdoc types for embed-proxy api client

--- bot/embed-proxy-client.js
+++ bot/embed-proxy-client.js
@@ -4,19 +4,40 @@ class EmbedProxyClient {
 		if (!this.endpoint) throw new Error('No embed proxy URL provided');
 	}
 
+	/**
+	 * @param src {string}
+	 * @param width {number}
+	 * @param height {number}
+	 * @returns {Promise<string>}
+	 */
 	async add(src, width, height) {
 		return await this.checkV2Live() ? await this.v2Add(src, width, height) : this.v1Url(src, width, height);
 	}
 
+	/**
+	 * @param src {string}
+	 * @param width {number}
+	 * @param height {number}
+	 * @returns {string}
+	 */
 	v1Url(src, width, height) {
 		return `${this.endpoint}/?src=${encodeURIComponent(src)}&width=${width}&height=${height}`;
 	}
 
+	/**
+	 * @returns {Promise<boolean>}
+	 */
 	async checkV2Live() {
 		const res = await fetch(`${this.endpoint}/v2/healthz`);
 		return res.ok;
 	}
 
+	/**
+	 * @param src {string}
+	 * @param width {number}
+	 * @param height {number}
+	 * @returns {Promise<string>}
+	 */
 	async v2Add(src, width, height) {
 		const res = await fetch(`${this.endpoint}/v2/add`, {
 			method: 'POST',
